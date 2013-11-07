61623cf5296606d36343568b3ed387c9e5c4b63f
Ramax Lo <ramaxlo@gmail.com>
2013-11-07 22:16:48 +0800

    urls.py: Remove employee_page url definition

--- lottery/urls.py
+++ lottery/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('lottery.views',
     url(r'^employee/$', 'employee'),
-    url(r'^employee_page/$', 'employee_page'),
     url(r'^prize/$', 'prize'),
     url(r'^prize_list/$', 'prize_list'),
     url(r'^prize_list_all/$', 'prize_list'),
