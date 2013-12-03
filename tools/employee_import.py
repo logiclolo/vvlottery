8862cbaee38b9077cf4e904f74c2e4172f064afb
Ramax Lo <ramaxlo@gmail.com>
2013-12-04 00:08:32 +0800

    Add importing tools
    These tools are used for importing employee & prize data from EXCEL files.

---
+++ tools/employee_import.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+
+import os, sys
+import xlrd
+import re
+
+sys.path.append(os.path.join(os.getenv('PWD'), '..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vvlottery.settings'
+
+from lottery.models import Employee, Department, Prize, Presenter
+
+sheet = 0
+
+def usage():
+	print 'Usage: employee_import.py <Excel file> [-n <sheet>]'
+	print '   -n <sheet>: The work sheet number (started from 0)'
+
+def get_department(depart):
+	try:
+		tmp = Department.objects.get(name = depart)
+		return tmp
+	except Department.DoesNotExist:
+		return None
+
+def add_department(depart_name):
+	tmp = Department()
+	tmp.name = depart_name
+	tmp.save()
+
+	return tmp
+
+def do_add_employee(name, jobid, depart_name):
+	employee = Employee()
+	employee.jobid = jobid
+	employee.name = name
+
+	depart = get_department(depart_name)
+	if not depart:
+		depart = add_department(depart_name)
+
+	employee.department = depart
+	employee.save()
+
+def add_employee(row):
+	department = row[0].value
+	jobid = row[1].value
+	name = row[2].value
+	exists = False
+
+	tmp = re.sub('^A0', 'vv', jobid)
+	conv_jobid = re.sub('^B0', 'va', tmp)
+#	print '%s,%s,%s' % (conv_jobid, name, department)
+
+	try:
+		employee = Employee.objects.get(jobid = conv_jobid)
+		print 'Skip %s:%s' % (jobid, name)
+		return False
+	except Employee.DoesNotExist:
+		do_add_employee(name, conv_jobid, department)
+
+	return True
+
+def employee_import(excel, sheet):
+	book = xlrd.open_workbook(excel)
+	sheet = book.sheet_by_index(sheet)
+	total = 0
+
+	for i in range(sheet.nrows):
+		if i == 0:
+			continue
+
+		row = sheet.row(i)
+		if add_employee(row):
+			total += 1
+
+	print 'imported %d entries' % total
+
+	return True
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage()
+		sys.exit(1)
+
+	excel = sys.argv[1]
+
+	if len(sys.argv) > 2:
+		if sys.argv[2] == '-n' and len(sys.argv) >= 3:
+			sheet = sys.argv[3]
+		else:
+			usage()
+			sys.exit(1)
+
+	if not employee_import(excel, 0):
+		sys.exit(1)
+
+	if not employee_import(excel, 1):
+		sys.exit(1)
---
+++ tools/prize_import.py
@@ -0,0 +1,102 @@
+#!/usr/bin/env python
+# vim: set encoding=utf-8
+
+import os, sys
+import xlrd
+import re
+
+sys.path.append(os.path.join(os.getenv('PWD'), '..'))
+os.environ['DJANGO_SETTINGS_MODULE'] = 'vvlottery.settings'
+
+from lottery.models import Employee, Department, Prize, Presenter, Phase
+
+sheet = 1
+phases = [u'員工獎', u'鹹魚翻身獎', u'地獄翻身獎']
+
+def usage():
+	print 'Usage: prize_import.py <Excel file>'
+
+def get_phase(phase):
+	try:
+		tmp = Phase.objects.get(name = phase)
+		return tmp
+	except Phase.DoesNotExist:
+		return None
+
+def add_phase(phase):
+	try:
+		tmp = Phase.objects.get(name = phase)
+		return tmp
+	except Phase.DoesNotExist:
+		tmp = Phase()
+		tmp.name = phase
+		tmp.save()
+
+	return tmp
+
+def do_add_prize(name, serial, get_on_site):
+	prize = Prize()
+	prize.serial = serial
+	prize.name = name
+	prize.onsite = get_on_site
+
+	phase = get_phase(phases[0])
+	if not phase:
+		print 'No phase found: %s' % phases[0]
+	
+	prize.phase = phase
+	prize.save()
+
+def add_prize(row):
+	field = row[0].value
+	serial = int(row[1].value)
+	name = row[2].value
+	exists = False
+
+	if field == '':
+		get_on_site = True
+	else:
+		get_on_site = False
+#	print '%s,%04d,%s' % ('true' if get_on_site else 'false', serial, name)
+
+	try:
+		prize = Prize.objects.get(serial = serial)
+		print 'Skip %d:%s' % (serial, name)
+		return False
+	except Prize.DoesNotExist:
+		do_add_prize(name, serial, get_on_site)
+
+	return True
+
+def prize_import(excel, sheet):
+	book = xlrd.open_workbook(excel)
+	sheet = book.sheet_by_index(sheet)
+	total = 0
+
+	for i in range(sheet.nrows):
+		if i == 0:
+			continue
+		elif i == (sheet.nrows - 1):
+			continue
+
+		row = sheet.row(i)
+		if add_prize(row):
+			total += 1
+
+	print 'imported %d entries' % total
+
+	return True
+
+if __name__ == '__main__':
+	if len(sys.argv) < 2:
+		usage()
+		sys.exit(1)
+
+	excel = sys.argv[1]
+
+	for p in phases:
+		add_phase(p)
+
+	if not prize_import(excel, sheet):
+		sys.exit(1)
+
