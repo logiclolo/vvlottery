4f83a78ecd7a0e0abf44c7df6b5d04d593dca11d
Ramax Lo <ramax.lo@vivotek.com>
2013-12-04 10:53:04 +0800

    Show data loading text in web page
    When loading a large set of data, show the 'loading data' text in the web
    page to inform the user.

--- lottery/static/lottery/employee_all.js
+++ lottery/static/lottery/employee_all.js
@@ -2,23 +2,27 @@ function query_employee_all(scope, http)
 {
 	http.get('/lottery/employee/').
 	success(function(data) {
+		scope.loaddata = false;
 		if (data.status == 'ok')
 		{
 			if (data.data.length == 0)
 			{
 				scope.employees = null;
 				scope.hasdata = false;
+				scope.nodata = true;
 			}
 			else
 			{
 				scope.employees = data.data;
 				scope.hasdata = true;
+				scope.nodata = false;
 			}
 		}
 		else if (data.status == 'error')
 		{
 			scope.employees = null;
 			scope.hasdata = false;
+			scope.nodata = true;
 		}
 	});
 }
@@ -26,5 +30,7 @@ function query_employee_all(scope, http)
 function employee_all_ctrl($scope, $http)
 {
 	$scope.hasdata = false;
+	$scope.nodata = false;
+	$scope.loaddata = true;
 	query_employee_all($scope, $http);
 }
