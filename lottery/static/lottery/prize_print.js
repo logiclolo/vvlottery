feca02ed56da3c89d2bfd714128e2506455a34da
Ramax Lo <ramax.lo@vivotek.com>
2014-01-03 10:32:03 +0800

    Add page to list employees without winning any prize
    This is used for printing a hard copy for later use.

--- lottery/static/lottery/prize_print.js
+++ lottery/static/lottery/prize_print.js
@@ -27,6 +27,35 @@ function query_by_phase(scope, http, phase)
 	});
 }
 
+function query_orphan_employee(scope, http)
+{
+	http.get('/lottery/employee/?orphan=1').
+	success(function(data) {
+		scope.loaddata = false;
+		if (data.status == 'ok')
+		{
+			if (data.data.length == 0)
+			{
+				scope.orphans = null;
+				scope.hasdata = false;
+				scope.nodata = true;
+			}
+			else
+			{
+				scope.orphans = data.data;
+				scope.hasdata = true;
+				scope.nodata = false;
+			}
+		}
+		else
+		{
+			scope.orphans = null;
+			scope.hasdata = false;
+			scope.nodata = true;
+		}
+	});
+}
+
 function prize_print($scope, $http)
 {
 	$scope.hasdata = false;
@@ -41,4 +70,12 @@ function prize_print2($scope, $http)
 	$scope.nodata = false;
 	$scope.loaddata = true;
 	query_by_phase($scope, $http, get_phase_alias(2));
+}
+
+function prize_print3($scope, $http)
+{
+	$scope.hasdata = false;
+	$scope.nodata = false;
+	$scope.loaddata = true;
+	query_orphan_employee($scope, $http);
 }
