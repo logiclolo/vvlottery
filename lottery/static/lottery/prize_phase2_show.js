690646cd399da73986073e26f3d4170bc4ea714e
Ramax Lo <ramax.lo@vivotek.com>
2014-01-17 17:34:12 +0800

    Add a page to show list of phase 2 prizes
    We use two-column layout to show all prizes donated. The maximum number of
    prizes is fixed to 36.

--- lottery/static/lottery/prize_phase2_show.js
+++ lottery/static/lottery/prize_phase2_show.js
@@ -1,3 +1,28 @@
+var max_len_part1 = 18;
+var max_total = 36;
+
+function init_prizes(scope)
+{
+	var tmp = [];
+	var tmp2 = [];
+
+	for (var i = 0; i < max_len_part1; i++)
+	{
+		var d = {donator: '', prize: ''};
+		tmp.push(d);
+	}
+
+	var len_part2 = max_total - max_len_part1;
+	for (var i = 0; i < len_part2; i++)
+	{
+		var d = {donator: '', prize: ''};
+		tmp2.push(d);
+	}
+
+	scope.prizes = tmp;
+	scope.prizes2 = tmp2;
+}
+
 function split_data(data)
 {
 	var ar = data.name.split(':');
@@ -20,24 +45,28 @@ function query_prizes(scope, http, timeout, phase)
 			if (data.data.length > 0)
 			{
 				var tmp = [];
+				var tmp2 = [];
+				data.data.reverse();
 
+				init_prizes(scope);
 				for (var i = 0; i < data.data.length; i++)
 				{
 					var d = split_data(data.data[i]);
-					tmp.push(d);
-				}
 
-				scope.prizes = tmp;
-			}
-			else
-			{
-				scope.prizes = null;
+					if (i < max_len_part1)
+						scope.prizes[i] = d;
+					else
+						scope.prizes2[i - max_len_part1] = d;
+				}
 			}
 		}
+		/*
 		else if (data.status == 'error')
 		{
 			scope.prizes = null;
+			scope.prizes2 = null;
 		}
+		*/
 
 		timeout(function () {
 			query_prizes(scope, http, timeout, phase);
